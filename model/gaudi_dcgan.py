9e327a30b9ab38a9dbc768262bc574f2cc63c584
Dustin Wilson <dmw2151@columbia.edu>
2022-02-02 23:33:26 -0500

    null extra stats

--- model/gaudi_dcgan.py
+++ model/gaudi_dcgan.py
@@ -418,7 +418,7 @@ def get_msls_dataloader(rank, train_cfg):
         "num_workers": 8,
         "pin_memory": True,
         "timeout": 0,
-        "prefetch_factor": 2,
+        "prefetch_factor": 4,
         "persistent_workers": False,
     }
 
@@ -589,7 +589,7 @@ def start_or_resume_training_run(
 
             # Calculate gradients for D in backward pass
             scaler_D.scale(err_D_real).backward()
-            D_X = torch.sigmoid(output).mean().item()
+            D_X = 1  # torch.sigmoid(output).mean().item()
 
             # (1.2) Update D Network; Train with All-fake batch
             fake = net_G(Z)
@@ -604,7 +604,7 @@ def start_or_resume_training_run(
                 err_D_fake = criterion(output, label)
 
             scaler_D.scale(err_D_fake).backward()
-            D_G_z1 = torch.sigmoid(output).mean().item()
+            D_G_z1 = 1  # torch.sigmoid(output).mean().item()
             err_D = err_D_real + err_D_fake
 
             # NOTE: This assumes we're using a custom Habana optimizer, in
@@ -630,7 +630,7 @@ def start_or_resume_training_run(
                 err_G = criterion(output, label)
 
             scaler_G.scale(err_G).backward()
-            D_G_z2 = torch.sigmoid(output).mean().item()
+            D_G_z2 = 1  # torch.sigmoid(output).mean().item()
 
             # Mark Habana Steps => Generator Optim;
             with MarkHTStep(HABANA_ENABLED and HABANA_LAZY):
